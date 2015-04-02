3b9e6b83259842f7ceb87d4a2daafb284a57e334
Michael Symmes <msymmes@gmail.com>
2015-04-02 07:28:18 -0400

    Corrected test #3 Added a functional process for adding all multiples of 3 or 5 between 0 and 1000.

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -62,12 +62,21 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
 
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+/* try chaining range() and reduce() */
+    var sum = _.range(1000).chain()
+      .reduce(function (accu, x) {
+        if (x % 3 == 0 || x % 5 == 0) {
+          return accu + x;
+        } else {
+          return accu;
+        }
+      })
+      .value();
 
     expect(233168).toBe(FILL_ME_IN);
   });
